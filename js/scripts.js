86cc53d1c73772728602714a1b16a5e2b4854b96
tlam22 <tszlam22@gmail.com>
2018-09-04 16:30:46 -0500

    updated profile with info

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,3 @@
-/*!
-    Title: Dev Portfolio Template
-    Version: 1.2.1
-    Last Change: 08/27/2017
-    Author: Ryan Fitzgerald
-    Repo: https://github.com/RyanFitzgerald/devportfolio-template
-    Issues: https://github.com/RyanFitzgerald/devportfolio-template/issues
-
-    Description: This file contains all the scripts associated with the single-page
-    portfolio website.
-*/
-
 (function($) {
 
     // Remove no-js class
@@ -58,12 +46,23 @@
 
         // Create each timeline block
         $userContent.each(function() {
-            $(this).addClass('vtimeline-content').wrap('<div class="vtimeline-point"><div class="vtimeline-block"></div></div>');
+            if($(this).hasClass("current_job")){
+                $(this).addClass('vtimeline-content').wrap('<div class="vtimeline-point current_job"><div class="vtimeline-block"></div></div>');
+            }else{
+                $(this).addClass('vtimeline-content').wrap('<div class="vtimeline-point"><div class="vtimeline-block"></div></div>');
+            }
+            
+            
         });
 
         // Add icons to each block
         $this.find('.vtimeline-point').each(function() {
-            $(this).prepend('<div class="vtimeline-icon"><i class="fa fa-map-marker"></i></div>');
+            if($(this).hasClass("current_job")){
+                $(this).prepend('<div class="vtimeline-icon" style="color: red;"><i class="fa fa-map-marker"></i></div>');
+            }else{
+                $(this).prepend('<div class="vtimeline-icon"><i class="fa fa-map-marker"></i></div>');
+            }
+           
         });
 
         // Add dates to the timeline if exists
